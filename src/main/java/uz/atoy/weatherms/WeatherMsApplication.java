ba0877961318b6616bd70a8ba559e9997d0e0743
ortukov.s <ali.artukov@gmail.com>
2022-12-13 19:00:41 +0500

    first commit

---
+++ src/main/java/uz/atoy/weatherms/WeatherMsApplication.java
@@ -0,0 +1,13 @@
+package uz.atoy.weatherms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherMsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/atoy/weatherms/WeatherMsApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.atoy.weatherms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
